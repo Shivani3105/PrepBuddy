163b73645bed93e521e9a5706c6734c32c3bf5bb
shivani gupta <93088306+Shivani3105@users.noreply.github.com>
2025-07-10 17:30:26 +0530

    Update index.js
    url changes

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,4 @@
-//const fs = require('fs');
 
-// // Step 1: Raw .env check
-// try {
-//   const envFileContent = fs.readFileSync('./.env', 'utf8');
-//   console.log("🧪 .env file content:\n" + envFileContent);
-// } catch (err) {
-//   console.error("❌ Couldn't read .env file:", err.message);
-// }
-
-// require('dotenv').config(); // Must come AFTER file is confirmed present
-
-// // Step 2: Print environment values
-// console.log("🔍 process.env.MONGO_URI:", process.env.MONGO_URI);
-// console.log("🔍 process.env.JWT_SECRET:", process.env.JWT_SECRET);
 
 
 require('dotenv').config();
@@ -25,5 +11,5 @@ app.get('/', (req, res) => {
     res.send("connection setup");
 });
 app.listen(port, () => {
-  console.log(`✅ Server running on http://192.168.1.37:${port}`);
+  console.log(`✅ Server running on ${port}`);
 });
