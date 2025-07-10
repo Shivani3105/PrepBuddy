bb044a12f521f2f7c2fa782a45d7bcd48f3fee58
shivani gupta <93088306+Shivani3105@users.noreply.github.com>
2025-07-10 17:32:09 +0530

    Update config.dart
    changes of url

--- lib/config.dart
+++ lib/config.dart
@@ -1,14 +1,15 @@
 // config.dart
 
-const String baseUrl = 'http://192.168.1.37:3000';
+const String baseUrl = 'https://prepbuddy-1.onrender.com'; 
+
 const String reg = '$baseUrl/registration';
-const String login='$baseUrl/login';
-const String storetodo='$baseUrl/storeTodo';
-const String gettodo='$baseUrl/getTodo';
-const String createQnA='$baseUrl/createQnA';
-const String getQnA='$baseUrl/getQnA';
+const String login = '$baseUrl/login';
+const String storetodo = '$baseUrl/storeTodo';
+const String gettodo = '$baseUrl/getTodo';
+const String createQnA = '$baseUrl/createQnA';
+const String getQnA = '$baseUrl/getQnA';
 String deleteQnA(String id) => '$baseUrl/deleteQnA/$id';
-const String editQnA='$baseUrl/editQnA';
-const String updateCountQnA='$baseUrl/updateCountQnA';
-const adminemail="guptashivani312002@gmail.com";
-//xWZaRUClqR7QFhqM
+const String editQnA = '$baseUrl/editQnA';
+const String updateCountQnA = '$baseUrl/updateCountQnA';
+
+const adminemail = "guptashivani312002@gmail.com";
